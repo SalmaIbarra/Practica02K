b08b0f75fa31dba813793bf896823c55b516fcee
SALMA YARETH IBARRA FLORES <2020030315@upsin.edu.mx>
2023-05-15 22:48:56 -0700

    Initial commit

---
+++ app/src/main/java/com/example/practica02k/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.practica02k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    val txtCmc = findViewById<EditText>(R.id.txtCmc) as EditText;
+    val txtKg=findViewById<EditText>(R.id.txtkg) as EditText;
+    val txtImc=findViewById<EditText>(R.id.txtimc)as EditText;
+    val btnCalcular=findViewById<Button>(R.id.btnCalcular) as Button;
+    val btnCerrar=findViewById<Button>(R.id.btnCerar) as Button;
+    val btnLimpiar=findViewById<Button>(R.id.btnLimpiar) as Button;
+
+    var cm=0.0;
+    var kg=0.0;
+    var imc=0.0;
+
+    btnLimpiar.setOnClickListener(View.OnClickListener {
+        txtKg.setText("");
+        txtImc.setText("");
+        txtCmc.setText("");
+    })
+
+
+    btnCalcular.setOnClickListener(View.OnClickListener {
+        if(txtCmc.text.toString().equals("") && txtKg.text.toString().equals(""))
+        {
+            Toast.makeText(this@MainActivity,"Hay campos vacios", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            cm=txtCmc.text.toString().toDouble();
+            kg=txtKg.text.toString().toDouble();
+            cm=cm/100;
+            val formato = DecimalFormat("#.##")
+            imc=kg/(cm*cm);
+            txtImc.setText((formato.format(imc)));
+
+        }
+
+    });
+
+
+
+    btnCerrar.setOnClickListener(View.OnClickListener {
+
+        finish();
+    })
+}
